12719b1f48afaa8037861d7c6c6028f33c01f8ac
Layanne <layannemenezes78@gmail.com>
2025-12-02 08:14:34 -0300

    comeco

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,1 +1,12 @@
-eu amo
+class Fone:
+    def __init__(self, id:str,number=str):
+        self.__id=id
+        self.__number=number
+
+    def getnum(self):
+        return self.__number
+    
+    def getid(self):
+        return self.__id
+
+    
